0136345e57530ed70cb811666050c1cbed5fae82
wecho12 <ehsankurd286@gmail.com>
2023-07-26 02:28:48 +0300

    Navbar toggle

--- src/assets/Data.js
+++ src/assets/Data.js
@@ -2,25 +2,29 @@ export const Navlink = [
   {
     id: 1,
     Title: "Home",
-    Style: "font-semibold  md:p-0 hover:text-jade-600",
+    Style:
+      "block px-3 py-2 rounded  hover:text-white hover:bg-jade-700 focus:outline-none focus:text-white focus:bg-jade-700",
     Link: "#",
   },
   {
     id: 2,
     Title: "Items",
-    Style: "font-semibold  md:p-0 hover:text-jade-600",
+    Style:
+      "block px-3 py-2 rounded  hover:text-white hover:bg-jade-700 focus:outline-none focus:text-white focus:bg-gray-700",
     Link: "#",
   },
   {
     id: 3,
     Title: "About",
-    Style: "font-semibold  md:p-0 hover:text-jade-600",
+    Style:
+      "block px-3 py-2 rounded hover:text-white hover:bg-jade-700 focus:outline-none focus:text-white focus:bg-jade-700",
     Link: "#",
   },
   {
     id: 4,
     Title: "Support",
-    Style: "font-semibold  md:p-0 hover:text-jade-600",
+    Style:
+      "block px-3 py-2 rounded hover:text-white hover:bg-jade-700 focus:outline-none focus:text-white focus:bg-jade-700",
     Link: "#",
   },
 ];
