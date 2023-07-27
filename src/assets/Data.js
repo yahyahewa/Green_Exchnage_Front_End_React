be76d23d67aae0ea5425b21682085368ad7d030c
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-07-27 20:18:14 +0300

    repair links data

--- src/assets/Data.js
+++ src/assets/Data.js
@@ -4,27 +4,27 @@ export const Navlink = [
     Title: "Home",
     Style:
       "block px-3 py-2 rounded active:text-jade-200  hover:text-white hover:text-jade-900 focus:outline-none focus:text-white focus:bg-jade-700",
-    Link: "#",
+    Link: "",
   },
   {
     id: 2,
     Title: "Items",
     Style:
       "block px-3 py-2 rounded active:text-jade-200 hover:text-white hover:text-jade-900 focus:outline-none focus:text-white focus:bg-jade-700",
-    Link: "#",
+    Link: "/items",
   },
   {
     id: 3,
     Title: "About",
     Style:
       "block px-3 py-2 rounded active:text-jade-200 hover:text-white hover:text-jade-900 focus:outline-none focus:text-white focus:bg-jade-700",
-    Link: "#",
+    Link: "/about",
   },
   {
     id: 4,
     Title: "Support",
     Style:
       "block px-3 py-2 rounded hover:text-white active:text-jade-200 hover:text-jade-900 focus:outline-none focus:text-white focus:bg-jade-700",
-    Link: "#",
+    Link: "/support",
   },
 ];
