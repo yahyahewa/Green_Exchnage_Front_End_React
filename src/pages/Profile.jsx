3747c9d32c5fa29cb6caf06f52f73c325a3f24ca
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-08-01 01:42:14 +0300

    create a page for profile

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+
+function Profile() {
+  return (
+    <>
+    <Navbar/><div className='text-6xl'>
+      profile
+    </div></>
+  )
+}
+
+export default Profile
