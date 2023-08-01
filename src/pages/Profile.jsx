7294fe3d29e582ae3a3520f1da9734cfbcdce575
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-08-01 03:08:29 +0300

    add logout button

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
-
+import { Link } from 'react-router-dom'
 function Profile() {
   return (
     <>
-    <Navbar/><div className='text-6xl'>
-      profile
-    </div></>
+    <Navbar/>
+    <Link to="/" className='text-white bg-jade-500 px-2 p-1 rounded' onClick={()=>{
+      localStorage.removeItem("userData");
+    }}>Logout</Link> </>
   )
 }
 
