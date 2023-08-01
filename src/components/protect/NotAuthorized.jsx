f1f3423d789430fa8a75a5b29d344e799c871adc
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-08-01 16:46:51 +0300

    change background colors

--- src/components/protect/NotAuthorized.jsx
+++ src/components/protect/NotAuthorized.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function NotAuthorized() {
-  return (  <div className="flex items-center justify-center h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+  return (  <div className="flex items-center justify-center h-screen bg-gradient-to-r from-jade-500 via-jade-700 to-jade-900">
   <div className="text-center">
     <h1 className="text-4xl font-bold text-white mb-4">Oops! You are not authorized.</h1>
     <p className="text-lg text-white mb-6">
       It seems you don't have the necessary permissions to access this page.
       Please contact the administrator for further assistance.
     </p>
-    <button
-      className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      onClick={() => {
-        // Handle the back-to-home action here
-      }}
+    <Link
+      className="bg-none border 
+       text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      to="/"
     >
       Back to Home
-    </button>
+    </Link>
   </div>
 </div>
   )
