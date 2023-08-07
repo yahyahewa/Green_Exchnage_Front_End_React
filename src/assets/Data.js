a02ab8f40bf4d9c56e3bf73ad6763d81e01142bf
wecho12 <ehsankurd286@gmail.com>
2023-08-08 02:21:18 +0300

    create a Card component

--- src/assets/Data.js
+++ src/assets/Data.js
@@ -28,3 +28,71 @@ export const Navlink = [
     Link: "/support",
   },
 ];
+
+export const Cards = [
+  {
+    id: 1,
+    DonatorName: "Ehsan Muhamad",
+    ProfileImg: "https://cdn.wallpapersafari.com/38/11/2WqNdH.jpg",
+    Date: "1/3/2023",
+    Location: "Khurmal",
+    Img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_4XQVtXdW-v1U7t4yJJ3xzwqt5Yz6FfriRBPpchS9D5RdkKDBHlY0no77pquW5TqsxMU&usqp=CAU",
+    Title: "clothes",
+    Desc: "wefnwei werp ower werpowe r weprm wepr weprowe",
+  },
+  {
+    id: 2,
+    DonatorName: "rozh bahman ",
+    ProfileImg:
+      "https://images.unsplash.com/photo-1594751543129-6701ad444259?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGFyayUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+    Date: "2/4/2022",
+    Location: "Halabja",
+    Img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_4XQVtXdW-v1U7t4yJJ3xzwqt5Yz6FfriRBPpchS9D5RdkKDBHlY0no77pquW5TqsxMU&usqp=CAU",
+    Title: "Shoes",
+    Desc: "Check website for latest pricing and availability. Images may be subject ",
+  },
+  {
+    id: 3,
+    DonatorName: "yahya hewa ",
+    ProfileImg:
+      "https://images.unsplash.com/photo-1594751543129-6701ad444259?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGFyayUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+    Date: "2/4/2022",
+    Location: "Halabja",
+    Img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_4XQVtXdW-v1U7t4yJJ3xzwqt5Yz6FfriRBPpchS9D5RdkKDBHlY0no77pquW5TqsxMU&usqp=CAU",
+    Title: "Shoes",
+    Desc: "Check website for latest pricing and availability. Images may be subject ",
+  },
+  {
+    id: 4,
+    DonatorName: "hevar tofiq ",
+    ProfileImg:
+      "https://image.lexica.art/full_jpg/acf5676d-e1bb-47cf-8e64-b171543de0ec",
+    Date: "16/3/2023",
+    Location: "Erbil",
+    Img: "https://solaroidenergy.com/wp-content/uploads/2023/02/second-hand-television-for-sale-in-johannesburg.jpg",
+    Title: "Tv",
+    Desc: "Check website for latest pricing and availability. Images may be subject ",
+  },
+  {
+    id: 5,
+    DonatorName: "Avin Osman",
+    ProfileImg:
+      "https://expertphotography.b-cdn.net/wp-content/uploads/2018/10/cool-profile-pictures-retouching-1.jpg",
+    Date: "36/7/2023",
+    Location: "Sulaymaniyah",
+    Img: "https://cdn.mos.cms.futurecdn.net/cvHGUKyMjbK2PifyRDrws4.jpg",
+    Title: "Camera",
+    Desc: "Check website for latest pricing and availability. Images may be subject ",
+  },
+  {
+    id: 6,
+    DonatorName: "Deya Salam",
+    ProfileImg:
+      "https://image.lexica.art/full_jpg/acf5676d-e1bb-47cf-8e64-b171543de0ec",
+    Date: "16/3/2023",
+    Location: "Erbil",
+    Img: "https://solaroidenergy.com/wp-content/uploads/2023/02/second-hand-television-for-sale-in-johannesburg.jpg",
+    Title: "Tv",
+    Desc: "Check website for latest pricing and availability. Images may be subject ",
+  },
+];
