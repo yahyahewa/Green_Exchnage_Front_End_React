0241c885c81ea94182c6f739eab77b74f940e87e
wecho12 <ehsankurd286@gmail.com>
2023-08-12 05:21:55 +0300

    create a blog section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Hero from "../components/Home/Hero";
 import Slideer from "../components/Home/Slideer";
+import Blogs from "../components/Home/Blogs";
 const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center ">
       <Hero />
+
       <Slideer />
+      <Blogs />
     </div>
   );
 };
