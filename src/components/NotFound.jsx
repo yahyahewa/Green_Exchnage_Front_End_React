826426295d19ada7e45ef4cb946cd265af0c6420
wecho12 <ehsankurd286@gmail.com>
2023-08-13 01:59:44 +0300

    fix issus in singleitem

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 const NotFound = () => {
-  return ( <div className="flex items-center justify-center h-screen bg-gradient-to-r from-jade-500 via-jade-700 to-jade-900">
-  <div className="text-center">
-    <h1 className="text-4xl font-bold text-white mb-4">404 Not Found.</h1>
-    <p className="text-lg text-white mb-6">
-    Oops! The page you are looking for does not exist.
-    </p>
-    <Link
-      className="bg-none border 
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-jade-500 via-jade-700 to-jade-900">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-white mb-4">404 Not Found.</h1>
+        <p className="text-lg text-white mb-6">
+          Oops! The page you are looking for does not exist.
+        </p>
+        <Link
+          className="bg-none border 
        text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      to="/"
-    >
-      Back to Home
-    </Link>
-  </div>
-</div>
+          to="/"
+        >
+          Back to Home
+        </Link>
+      </div>
+    </div>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Banner from "../components/FilterAndSearch/Banner";
 import Card from "../components/card/Card";
 import Navbar from "../components/navbar/Navbar";
+import SingleItem from "./SingleItem";
 function Home() {
   return (
     <>
       <Navbar />
       <Banner />
       <Card />
-      <SingleI
+      <SingleItem />
     </>
   );
 }
