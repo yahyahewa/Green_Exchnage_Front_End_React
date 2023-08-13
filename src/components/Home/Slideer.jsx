0b42ba618329ab2d90ac9daa6e458ef8ddd06a63
wecho12 <ehsankurd286@gmail.com>
2023-08-13 03:51:49 +0300

    some changes

--- src/components/Home/Slideer.jsx
+++ src/components/Home/Slideer.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Slideer = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-2 mt-4">
+    <div className="flex flex-col justify-center items-center gap-2 md:pt-24 pt-8">
       <img
         className=" w-20 rounded-ee-3xl rounded-ss-3xl shadow-md shadow-jade-500"
         src="https://images.unsplash.com/photo-1614204424926-196a80bf0be8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
       />
 
       <h3 className="font-bold text-neutral-600 ">Shnyar Abdulrahman</h3>
-      <span className="mt-1 bg-jade-500/80 px-4 text-sm py-1 rounded-xl text-white capitalize">
+      <span className=" border-2 border-jade-500 px-4 text-sm py-1 rounded-2xl text-jade-600 capitalize">
         for donate
       </span>
       <img
