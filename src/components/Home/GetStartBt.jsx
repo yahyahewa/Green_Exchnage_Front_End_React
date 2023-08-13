135e7e08fb630ccb222a64c0dc9ff8eed2bb9349
wecho12 <ehsankurd286@gmail.com>
2023-08-13 03:30:09 +0300

    some change in donate section

--- src/components/Home/GetStartBt.jsx
+++ src/components/Home/GetStartBt.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { HiChevronRight } from "react-icons/hi";
 const GetStartBt = () => {
   const [isLogin, setIsLogin] = useState(false);
 
@@ -12,21 +12,9 @@ const GetStartBt = () => {
           className="relative inline-flex items-center px-16 py-2 md:m-6 overflow-hidden text-lg font-medium text-jade-600 border-2 border-jade-600 rounded-full hover:text-white group hover:bg-gray-50"
         >
           <span className="absolute left-0 block w-full h-0 transition-all bg-jade-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+
           <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
-            <svg
-              className="w-5 h-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M14 5l7 7m0 0l-7 7m7-7H3"
-              ></path>
-            </svg>
+            <HiChevronRight className="w-6 h-6" />
           </span>
           <span className="relative">Go to Dashboard</span>
         </Link>
@@ -37,20 +25,7 @@ const GetStartBt = () => {
         >
           <span className="absolute left-0 block w-full h-0 transition-all bg-jade-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
           <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
-            <svg
-              className="w-5 h-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M14 5l7 7m0 0l-7 7m7-7H3"
-              ></path>
-            </svg>
+            <HiChevronRight className="w-6 h-6" />
           </span>
           <span className="relative">Get Started</span>
         </Link>
