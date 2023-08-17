2204c336bdee731a9c33c8845a9c1f493967d86b
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-08-17 17:21:10 +0300

    fix conflectiong

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,16 +1,9 @@
-
-
 const Hero = () => {
   return (
-    <section className="pt-14 flex items-center flex-col">
+    <section className="pt- flex items-center flex-col">
       <div className="flex flex-col items-center justify-center md:pt-24 pt-8">
-<<<<<<< HEAD
-        <h1 className="text-3xl sm:text-4xl font-extrabold text-center  from-gray-800 ">
-          One person's Trash is another person's Treasure
-=======
         <h1 className="text-3xl sm:text-4xl font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-gray-700 to-gray-400">
           One persons Trash is another persons Treasure
->>>>>>> main
         </h1>
         <p className="mt-4 px-4 pb-4 text-md  text-center text-lg  text-gray-900">
           You protect the environment by giving what you no longer need
