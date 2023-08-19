2c7d73ff2ace97d1b144260f27cc45f3d8b8c61e
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-08-20 02:39:35 +0300

    complete category and subcategory

--- src/app/api/profile.js
+++ src/app/api/profile.js
@@ -1,27 +1,25 @@
 import { api } from "./api";
 
-const userApi=api.injectEndpoints({
-    endpoints:(builder)=>({
-        uploads:builder.mutation({
-            query:(files,token)=>{
-                const formData=new FormData();
-                if(files.length>0){
-                    for(let file of files){
-                        formData.append("photos",file)
-                    }
+const userApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    uploads: builder.mutation({
+      query: (files, token) => {
+        const formData = new FormData();
+        if (files.length > 0) {
+          for (let file of files) {
+            formData.append("photos", file);
+          }
 
-                    return{
-                        url:"api/user/uploads",
-                        method:"POST",
-                        headers:{Authorization: `Bearer ${token}`},
-                        body:formData
-                    }
-                }
-            }
-        })
-    })
-})
+          return {
+            url: "api/product/uploads",
+            method: "POST",
+            headers: { Authorization: `Bearer ${token}` },
+            body: formData,
+          };
+        }
+      },
+    }),
+  }),
+});
 
-export const {
-    useUploadsMutation
-}=userApi;
+export const { useUploadsMutation } = userApi;
