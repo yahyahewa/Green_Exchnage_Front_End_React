7e6197d553ce58e37cc4f311b4fe61c2a7370ee3
wecho12 <ehsankurd286@gmail.com>
2023-08-27 04:59:22 +0300

    .

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -2,10 +2,10 @@ const Hero = () => {
   return (
     <section className="pt- flex items-center flex-col">
       <div className="flex flex-col items-center justify-center md:pt-24 pt-8">
-        <h1 className="lg:text-4xl   leading-9 text-gray-800  pb-4 text-3xl px-5 sm:text-4xl text-to font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-neutral-800 to-neutral-400">
+        <h1 className="pb-4 text-3xl sm:text-4xl font-extrabold text-center  from-gray-800">
           One persons Trash is another persons Treasure
         </h1>
-        <p className="mt-4 px-4 pb-4 text-md font-normal  text-center text-lg  text-gray-900">
+        <p className="mt-4 px-4 pb-4 text-md   text-center    font-normal text-md  leading-6 text-lg  text-gray-900 ">
           You protect the environment by giving what you no longer need
         </p>
       </div>
