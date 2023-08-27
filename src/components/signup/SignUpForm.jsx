57448a7d50a029f54d5be1ad5cdd17578ee741ff
Sana Rzgar <sanarizgar34@gmail.com>
2023-08-27 21:55:47 +0300

    signup : responsive

--- src/components/signup/SignUpForm.jsx
+++ src/components/signup/SignUpForm.jsx
@@ -31,13 +31,13 @@ function SignUpForm() {
           )
         : setThereUser("check your information");
     }
-  }, [data]);
+  }, [data, isError, isLoading]);
 
   if (data?.status == "success" && data?.data?.token)
     return <Navigate to="/profile" replace />;
   return (
     <Formik>
-      <div className="w-full flex  flex-col items-center justify-center mt-28">
+      <div className="w-full flex  flex-col items-center justify-center md:mt-16 lg:mt-14 xl:mt-28">
         <p className="font-semibold text-lg text-gray-800 ">Create Account </p>
         <Form
           onSubmit={formik.handleSubmit}
