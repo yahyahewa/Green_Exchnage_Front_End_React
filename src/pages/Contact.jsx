026574dc6a73dea60a9a374dc3293d94edc182f7
Sana Rzgar <sanarizgar34@gmail.com>
2023-08-27 21:48:55 +0300

    login page :responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import contact from "../assets/images/contact.svg"
 import ContactForm from "../components/contact/ContactForm";
 function Contact() {
-  return  <div className="px-6 sm:px-12 lg:mx-26 md:px-20 pt-10 grid  md:grid-cols-2 items-start gap-20 h-screen">
+  return  <div className="px-6 sm:px-12 lg:mx-26 md:px-20 pt-10 grid  md:grid-cols-2 items-start gap-20 min-h-screen">
     <div className="flex justify-center">
       <img src={contact} className="w-full h-72 md:h-[500px] object-contain sm:object-cover" />
     </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,8 +12,8 @@ function Login() {
   return (
     <>
   
-      <div className="lg:mx-36 pt-10 grid grid-cols-2 items-start gap-20 h-screen">
-        <div className="flex justify-center"><img src={ login} className="w-full h-[500px] object-cover" /></div>
+      <div className="px-6 sm:px-12 lg:mx-26 md:px-20 pt-10 grid  md:grid-cols-2 items-start gap-20 min-h-screen">
+        <div className="flex justify-center"><img src={ login} className="w-full h-72 sm:h-96 md:h-[500px] object-contain md:object-cover" /></div>
         <LoginForm/>
          
        </div>
