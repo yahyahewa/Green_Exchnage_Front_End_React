6ab726691d3b3c6569b656499946d7ef8d4f9962
Sana Rzgar <sanarizgar34@gmail.com>
2023-08-27 21:40:10 +0300

    contact form :responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import ContactForm from "../components/contact/ContactForm";
 function Contact() {
   return  <div className="px-6 sm:px-12 lg:mx-26 md:px-20 pt-10 grid  md:grid-cols-2 items-start gap-20 h-screen">
     <div className="flex justify-center">
-      <img src={contact} className="w-full h-72 md:h-[500px] object-contain md:object-cover" />
+      <img src={contact} className="w-full h-72 md:h-[500px] object-contain sm:object-cover" />
     </div>
     <ContactForm/>
        </div>;
