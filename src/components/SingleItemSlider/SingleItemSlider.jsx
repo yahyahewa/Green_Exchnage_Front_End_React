853c34eed676788745509380d3991c564953b4c0
Sana Rzgar <sanarizgar34@gmail.com>
2023-09-07 16:22:20 +0300

    add : slider

---
+++ src/components/SingleItemSlider/SingleItemSlider.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+function SingleItemSlider() {
+     const images=["https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1999&q=80","https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80", "https://plus.unsplash.com/premium_photo-1679913792906-13ccc5c84d44?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80","https://images.unsplash.com/photo-1581235720704-06d3acfcb36f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80", "https://images.unsplash.com/photo-1564466809058-bf4114d55352?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1886&q=80"]
+//      const images = [
+//           {
+//                id: 1,
+//                url: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1999&q=80"
+//           },
+//            {
+//                id: 2,
+//                url: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80"
+//           },
+//             {
+//                id: 3,
+//                url: "https://plus.unsplash.com/premium_photo-1679913792906-13ccc5c84d44?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+//           },
+//              {
+//                id: 4,
+//                url: "https://images.unsplash.com/photo-1581235720704-06d3acfcb36f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80"
+//           },
+//               {
+//                id: 5,
+//                url: "https://images.unsplash.com/photo-1564466809058-bf4114d55352?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1886&q=80"
+//           }
+              
+//  ]
+
+       const [wordData, setWordData] = useState(images[0]);
+  const handleClick = (i) => {
+    const wordSlider = images[i];
+    setWordData(wordSlider);
+  };
+  return (
+     <div className="h-[500px] ">
+      <img
+        src={wordData || images[0]}
+        alt=""
+        className="w-full h-[400px] object-cover"
+      />
+      <div className="flex w-full justify-between">
+        {images.map((data, i) => (
+          <div className="mt-5 relative overflow-hidden" key={i}>
+            <img
+              alt=""
+              className={
+                (wordData[i] === i ? " " : "") +
+                "w-[110px] h-[90px] object-cover hover:scale-110 duration-500 hover:duration-500 "
+              }
+              src={data}
+              onClick={() => handleClick(i)}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default SingleItemSlider
--- src/pages/SingleItem.jsx
+++ src/pages/SingleItem.jsx
@@ -1,34 +1,40 @@
 // import React, { useState } from "react";
-import {  useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { CgCalendarDates } from "react-icons/cg";
+import { IoLocationSharp } from "react-icons/io5";
+
+import SingleItemSlider from "../components/SingleItemSlider/SingleItemSlider";
 function SingleItem() {
   const { id } = useParams()
   console.log(id)
   // const [islogin, setIsLoggedIn] = useState(true);
   return (
     <>
-      <section className="px-6 sm:px-12 lg:mx-26 md:px-20 text-gray-8 ">
-        <div className="py-32 grid grid-cols-2 gap-5 lg:gap-x-10">
-        
-            <img
+      <section className="px-6 sm:px-12 lg:mx-26 md:px-20 flex text-neutral-500">
+        <div className="py-32 grid md:grid-cols-2 gap-5 lg:gap-x-10">
+        <SingleItemSlider/>
+            {/* <img
               alt={""}
               className="w-full h-[500px] object-cover rounded"
               src="https://codecondo.com/wp-content/uploads/2016/11/Metasploit-The-Penetration-Testers-Guide-e1598449869321.png"
           />
-          
-          <div className="flex flex-col justify-between xl:w-3/4">
+           */}
+          <div className="flex flex-col justify-between xl:w-3/4 ">
             <div className="flex justify-between items-end">
               <div >
-              <h1 className="font-semibold text-3xl ">
+              <h1 className="font-semibold text-3xl text-neutral-600 ">
                 Book
-              </h1>
-              <h2 className="text-xl font-semibold mt-2">
+                </h1>
+                <p className="text-xl font-semibold mt-2">Category</p>
+                 <h2 className="text-xl font-semibold mt-2">
                 Ehsan muhamad
-              </h2>
+                 </h2>
             </div>
             
              <div className=" flex  ">
-                <div>1/1/2023</div>
-                <div className="ml-4">khurmal</div>
+                <div className="flex items-center"><CgCalendarDates className="w-6 h-6" /> <span className="ml-2"> 1/1/2023</span></div>
+                    
+                <div className="ml-5 flex items-center">  <IoLocationSharp className="mr-2 w-5 h-5" /><span>khurmal</span> </div>
               
                     
              
@@ -36,16 +42,16 @@ function SingleItem() {
             </div>
            
              
-              <div className="font-semibold text-xl mt-5">
+              {/* <div className="font-semibold text-xl ">
                 Description
-              </div>
+              </div> */}
               <p className="mt-2">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Quaerat corporis labore accusantium placeat dicta eos vero earum
                 tempora iure, ipsam pariatur adipisci? Sit, rerum nam sequi
                 ratione placeat harum aliquid?
               </p>
-              <div className="">
+              {/* <div className="">
                 <div className="font-semibold text-xl mt-5">
                   Note
                 </div>
@@ -55,8 +61,18 @@ function SingleItem() {
                   earum tempora iure, ipsam pariatur adipisci? Sit, rerum nam
                   sequi ratione placeat harum aliquid?
                 </p>
+            </div> */}
+            <div className="flex flex-col">
+              <div className="flex ">
+                <span>Owner Phone number :</span>
+                <span>0770 111 11 11</span>
+              </div>
+              <div className="flex ">
+                <span>employee phone number :</span>
+                <span>0770 111 11 11</span>
+              </div>
             </div>
-            <button className="text-white bg-green py-2  rounded hover:bg-opacity-80 hover:duration-500 duration-500">Order</button>
+            {/* <button className="text-white bg-green py-2  rounded hover:bg-opacity-80 hover:duration-500 duration-500">Order</button> */}
             </div>
           </div>
       </section>
