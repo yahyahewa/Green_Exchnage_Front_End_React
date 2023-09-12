962daa09129974008e973d2c04efd640002a0a21
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-09-13 01:41:30 +0300

    create api request for cities

---
+++ src/app/api/city.js
@@ -0,0 +1,11 @@
+import { api } from "./api";
+
+const cityApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getCity: builder.query({
+      query: () => `api/city`,
+    }),
+  }),
+});
+
+export const { useGetCityQuery } = cityApi;
