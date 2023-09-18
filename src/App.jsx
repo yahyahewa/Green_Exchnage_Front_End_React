3a7ba5d20b99dd4d51229b36bce1a1da32ab190d
Sana Rzgar <sanarizgar34@gmail.com>
2023-09-18 20:40:26 +0300

    navbar fixed : profile imagae

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-
-import "./index.css";
+import './index.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import 'react-loading-skeleton/dist/skeleton.css'
+import 'react-loading-skeleton/dist/skeleton.css';
 // Components
-import Router from "./pages/Router/Router";
-import { SkeletonTheme } from "react-loading-skeleton";
+import Router from './pages/Router/Router';
+import { SkeletonTheme } from 'react-loading-skeleton';
 function App() {
   return (
-     <SkeletonTheme baseColor="#ebe8e8" highlightColor="#1d905150">
-    <div className="min-h-screen">
-      {/* <Navbar/> */}
-      <Router />
-      {/* <Fotter/> */}
+    <SkeletonTheme baseColor="#ebe8e8" highlightColor="#1d905130">
+      <div className="min-h-screen">
+        {/* <Navbar/> */}
+        <Router />
+        {/* <Fotter/> */}
       </div>
     </SkeletonTheme>
   );
